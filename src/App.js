1a820a757906d2484472fa658d453e3f887946a0
Daniel <dev.dani.alves@gmail.com>
2023-09-14 16:24:13 -0400

    sim

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
 
 import Login from './screens/login/login';
+
 import HomePage from './screens/homePage/homePage';
+
+import PatrimonioPage from './screens/patrimonio/patrimonio';
+
 function App() {
   return (
     <Router>
       <Routes>
       <Route index path="/" element={<Login/>} />
       <Route index path="/home" element={<HomePage/>} />
+      <Route index path="/patrimonio" element={<PatrimonioPage/>} />
 
       </Routes>
         
